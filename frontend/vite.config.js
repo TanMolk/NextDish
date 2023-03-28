a409f238583c91d46093e81beaedacaad5f7ce89
Wei <W.Tan8@newcastle.ac.uk>
2023-03-28 14:02:33 +0100

    1. Update build setting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,20 @@ import NutUIResolver from '@nutui/nutui/dist/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        //server port
+        port: 5173,
+        strictPort: true,
+        //only work in development, avoid cross-domain; After building, it doesn't work
+        proxy: {
+            '/map-api': {
+                target: 'https://maps.googleapis.com/maps/api',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/map-api/, '')
+            }
+        }
+    },
+    base: "./",
     plugins: [
         vue(),
         // import un-plugin auto import
@@ -26,14 +40,33 @@ export default defineConfig({
             }
         }
     },
-    base: "./",
-    server: {
-        proxy: {
-            '/map-api': {
-                target: 'https://maps.googleapis.com/maps/api',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/map-api/, '')
-            }
+    // output setting
+    build: {
+        chunkSizeWarningLimit: 1500,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        const arr = id.toString().split('node_modules/')[1].split('/')
+                        switch (arr[0]) {
+                            case 'vue':
+                            case '@nutui/nutui':
+                            case 'element-plus':
+                            case 'vue3-google-map':
+                            case 'vue3-infinite-list':
+                                return '_' + arr[0]
+                            default :
+                                return '__vendor'
+                        }
+                    }
+                },
+                chunkFileNames: 'static/chunk/[name]-[hash].js',
+                entryFileNames: 'static/main/[name]-[hash].js',
+                assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+            },
+            brotliSize: false,
+            target: 'esnext',
+            minify: 'esbuild'
         }
     }
-})
+});
